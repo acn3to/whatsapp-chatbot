12b44d2682cfccb7c0b8e53377872bdd53f63cd7
acn3to <costanetoow@gmail.com>
2022-12-07 02:34:30 -0300

    :sparkles: feat: creates messages route

--- api/server.js
+++ api/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+app.use(express.json())
 
 const port = process.env.PORT || 3000
+
+const messagesRoutes = require('../routes/messages')
+
+app.use(messagesRoutes)
 
 app.get('/', (req, res) => {
   res.send('Home page!')
